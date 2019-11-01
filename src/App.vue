ec5755bf79ecc96868b8ba07926a63860edbac96
jackbrogan <jack.brogan@digit-hull.com>
2019-11-01 20:20:29 +0000

    Added custom template using the router link

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,67 @@
 <template>
-  <v-app>
-    <v-app-bar app>
+  <v-app id="talli">
+    <v-navigation-drawer
+      v-model="drawer" app
+    >
+      <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.link"
+        >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+
+        </v-list-item>
+      </v-list>
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block>Logout</v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      color="green"
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"/>
       <v-toolbar-title class="headline text-uppercase">
-        <span>/r/</span>
-        <span class="font-weight-light">tea</span>
+        <span>/r/tea</span>
+        <span class="font-weight-light"> vendor vote</span>
       </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view/>
     </v-content>
+    <v-footer
+      color="green"
+      app
+    >
+      <span class="white--text">made by /r/tea contributors &copy; 2019</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data: () => ({
-    //
-  })
+  data () {
+    return {
+      drawer: false,
+      items: [
+        { title: 'Dashboard', icon: 'mdi-home', link: '/' },
+        { title: 'Account', icon: 'mdi-account-box', link: 'about' },
+        { title: 'Admin', icon: 'mdi-gavel', link: 'nothing' }
+      ]
+    }
+  }
 }
 </script>
